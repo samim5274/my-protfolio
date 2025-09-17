baa121ab9d91ccf3d21bb54369a12129911774ee
samim5274 <banglarcele1122@gmail.com>
2025-09-17 17:07:29 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
     <div class="bg-[#111827] min-h-screen">
-        <Suspense>
-            <template #default>
-                <div>
-                    <Navbar />
-                    <HeroSection />
-                    <Services />
-                    <AboutSection />
-                    <ExperienceAndSkills />
-                    <LatestProject />
-                    <Testimonial />
-                    <Contact />
-                    <Footer />
-                    <BackToTop />                    
-                </div>
-            </template>
-            <template #fallback>
-                <div class="flex justify-center items-center min-h-screen">
-                    <Spinner />
-                </div>
-            </template>
-        </Suspense>
+        <div>
+            <Navbar />
+            <HeroSection />
+            <Services />
+            <AboutSection />
+            <ExperienceAndSkills />
+            <LatestProject />
+            <Contact />
+            <Testimonial />
+            <Footer />
+            <BackToTop />                    
+        </div>
       <!-- <p>https://www.youtube.com/watch?v=U10h8rrPe6g</p> -->
     </div>
 </template>
@@ -39,7 +30,7 @@ import Contact from './components/Contact.vue';
 import Testimonial from './components/Testimonial.vue';
 import Footer from './components/Footer.vue';
 import BackToTop from './components/BackToTop.vue';
-import Spinner from './components/Spinner.vue';
+import LatestProject from './components/LatestProject.vue';
 
 onMounted(()=>{
   AOS.init();
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -50,7 +50,7 @@ const Menu =ref([
   {name:"Services", href:'#services'},
   {name:"About Me", href:'#about'},
   {name:"Skills", href:'#skills'},
-  {name:"Projects", href:'#project'},
+  {name:"Projects", href:'#projects'},
   {name:"Testimonials", href:'#testimonials'},
   {name:"Contact", href:'#contact'},
 ]);
